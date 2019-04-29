f19dae2dadbd8c181d60eee552365f9b82185cdf
Venelin Efremov <ghent360@iqury.us>
2019-04-28 21:22:23 -0700

    Update robot program for a 2x20 header.

--- runner/main.cc
+++ runner/main.cc
@@ -1,4 +1,5 @@
 #include "CommandRunner.h"
+#include <unistd.h>
 
 int main() {
     CommandRunner r("/dev/ttyACM0");
@@ -13,11 +14,37 @@ int main() {
     r.sendAndAck("G28 XY");
     printf("Home Z\n");
     r.sendAndAck("G28 Z");
-    r.sendAndAck("G1 Z3 F200");
+    r.sendAndAck("M106 P2 S255");
+    r.sendAndAck("G90");
+    r.sendAndAck("G1 Z9 F800");
     r.sendAndAck("M400");
-    r.sendAndAck("G1 X60 Y50 F2000");
+    //r.sendAndAck("G1 X51.67 Y113.5 F10000");
+    r.sendAndAck("G1 X54.14 Y113.5 F10000");
     r.sendAndAck("M400");
-    r.sendAndAck("G1 X0 Y100 F700");
-    r.sendAndAck("M400");
+    r.sendAndAck("G91");
+    for (int i=0; i < 20; i++) {
+        r.sendAndAck("G1 Z-2.5 F800");
+        r.sendAndAck("G1 Z-0.5 F200");
+        r.sendAndAck("G1 X0.5 Z-0.4 F100");
+        r.sendAndAck("M400");
+        usleep(750000);
+        r.sendAndAck("G1 E7 F800");
+        r.sendAndAck("M400");
+        r.sendAndAck("G1 E2 F200");
+        r.sendAndAck("M400");
+        sleep(3);
+        r.sendAndAck("G1 E1 F200");
+        r.sendAndAck("M400");
+        r.sendAndAck("G1 E-7.5 F2000");
+        r.sendAndAck("M400");
+        usleep(500000);
+        r.sendAndAck("G1 X-0.5 Z3.4 F800");
+        r.sendAndAck("M400");
+        r.sendAndAck("G1 Y2.54 F5000");
+        r.sendAndAck("M400");
+    }
+    r.sendAndAck("G90");
+    r.sendAndAck("G1 X0 Y0 F10000");
+    //r.sendAndAck("M106 P2 S0");
     r.sendAndAck("M18");
 }
